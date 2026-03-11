a87724e17b8c241904b3f49a5df6901269c9ff2b
danett <danett.pepito@mds.ac.nz>
2026-03-11 20:46:46 +1300

    Changed position of spiral
    Flattened circle, changing y axis

--- sketch.js
+++ sketch.js
@@ -1,29 +1,46 @@
-let t = 0;
+let r = 0.2;    // starting radius
+let dr = 0.8;   // radius increment
+let th = 0;     // starting rotation
+let dth = 0.06; // rotation increment
 
 function setup() {
   createCanvas(600, 400);
-  stroke(255);
+  strokeWeight(0.5);
   noFill();
 }
 
 function draw() {
-  background(20);
+  background(281, 261, 249);
   translate(width/2, height/2);
 
-  strokeWeight(1);
+  let rTemp = r; // reset radius for this frame
+  th = 0;        // reset angle
 
   beginShape();
-  for (let i = 0; i < 400; i++) {
+  let len = 8000;
+  for (let i = 0; i < len; i++) {
+    rTemp += (dr + i/10000) * dth/(2*PI);
+    th += dth;
 
-    let a = i * 0.05;
-    let r = 120 * sin(a * 0.5);
+    let x = rTemp * cos(th);
+    let y = rTemp * sin(th) * 0.4; // sits vertically
 
-    let x = r * cos(a + t);
-    let y = r * sin(a);
+    // noise-based wavy movement
+    let phi = noise(x/100, y/100, frameCount*0.005) * TWO_PI;
+    let v = ((rTemp-20)**1.2)/5;
 
-    vertex(x, y);
+    // optional: skip some vertices to reduce lines
+    if (i % 3 === 0) {
+      vertex(
+        x + v*cos(phi) + sin(i/1000 + frameCount*0.01)*10,
+        // change rotation movement
+        y + v*sin(phi) + cos(i/1200 + frameCount*0.01)*10
+      );
+    }
   }
   endShape();
+}
 
-  t += 0.01;
+function mouseReleased(){
+  r = 0.1;
 }
